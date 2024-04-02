32c21f0e59137963e78834a4b608239af5003a36
giftea <eleojogiftuhiene@gmail.com>
2024-04-02 02:51:46 +0100

    added logo

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,8 @@
+import Header from "@/components/header";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className=""></main>;
+  return <main className="">
+    <Header />
+  </main>;
 }
---
+++ frontend/components/header/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src="/images/logo.jpg" width={150} />
+    </div>
+  );
+};
+
+export default Navbar;
