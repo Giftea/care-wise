b2cf23cc229c2a9acc5c52eb307527757facdef6
giftea <eleojogiftuhiene@gmail.com>
2024-04-18 11:39:09 +0100

    added doctor card

--- frontend/app/doctors/find-doctors/page.js
+++ frontend/app/doctors/find-doctors/page.js
@@ -2,7 +2,13 @@ import FindDoctors from "@/components/common/doctor/FindDoctors";
 import React from "react";
 
 const FindAllDoctors = () => {
-  return <FindDoctors />;
+  return <>
+  <div className="bg-primary text-white py-10 text-center">
+
+    <p className="text-5xl font-semibold">HEALTHCARE PROVIDERS</p>
+  </div>
+  <FindDoctors />
+  </>;
 };
 
 export default FindAllDoctors;
--- frontend/components/common/doctor/FindDoctors.jsx
+++ frontend/components/common/doctor/FindDoctors.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const FindDoctors = () => {
   return (
-    <div>FindDoctors</div>
-  )
-}
+    <div className="section-padding py-10">
+      <p>
+        Find one of the best Doctors to get instant medical advice and second
+        opinion for your health problems. Ask doctors online and consult them
+        through video, phone, or chat. Get started now!
+      </p>
 
-export default FindDoctors
+      <div className="grid grid-cols-3">
+        <DoctorCard />
+        <DoctorCard />
+      </div>
+    </div>
+  );
+};
+
+export default FindDoctors;
+
+const DoctorCard = () => {
+  return (
+    <div className="mt-5 bg-white shadow p-8 col-span-2 flex justify-between rounded">
+      <div className="flex">
+        <Avatar className="w-[100px] h-[100px]">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+
+        <div className="ml-5">
+          <p className="text-primary text-2xl font-semibold">Dr. John</p>
+          <p className="text-textgrey mb-5">MBBS</p>
+          <p>
+            Experience: <span className="font-semibold">16+ Yrs</span>
+          </p>
+          <p>
+            Consulting Languages: <span className="font-semibold">English</span>{" "}
+          </p>
+          <div className="mt-5 flex">
+            <span className="border-2 border-[#4999fa53] text-secondary p-2 mr-2 rounded">
+              Family Physician
+            </span>
+            <span className="border-2 border-[#4999fa53] text-secondary p-2 mr-2 rounded">
+              Family Physician
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <button className="bg-primary text-white p-2 mr-2 rounded my-1 px-10">
+          Ask Query $3
+        </button>
+        <button className="bg-secondary text-white p-2 mr-2 rounded my-1 px-10">
+          50hrs Chat $9
+        </button>
+        <button className="bg-textgrey text-white p-2 mr-2 rounded my-1 px-10">
+          Consult $13
+        </button>
+      </div>
+    </div>
+  );
+};
